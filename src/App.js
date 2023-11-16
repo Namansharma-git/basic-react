617cfe70d8a054d5936e3617ba4f25030f05a95d
Namansharma-git <65589832+Namansharma-git@users.noreply.github.com>
2023-11-16 14:32:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Chai from "./Chai";
+
+function App() {
+  return (
+    <>
+    <Chai/>
+    {/* <h1>CODE WITH CHAI | NAMAN SHARMA </h1> */}
+    </>
+  );
+}
+
+export default App;
+  
---
+++ src/Chai.js
@@ -0,0 +1,7 @@
+function Chai() {
+    return (
+        <h2> Chai idr bhi tyarr hai </h2>
+    )
+}
+
+export default  Chai 
